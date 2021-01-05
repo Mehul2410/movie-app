8628935849b980410b33691a0a699e5f8f27f961
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2021-01-05 10:19:44 +0530

    api setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 // const { parse } = require("url");
 const next = require("next");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -9,6 +10,37 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  server.use(bodyParser.json());
+
+  server.get("/api/v1/movies", (req, res) => {
+    return res.json({ message: "Hello World" });
+  });
+
+  server.post("/api/v1/movies", (req, res) => {
+    const movie = req.body;
+    console.log(JSON.stringify(movie));
+    return res.json({ ...movie, createdTime: "today", autor: "mehul" });
+  });
+  server.patch("/api/v1/movies/:id", (req, res) => {
+    const { id } = req.params;
+    res.json({ message: ` updating post id;${id}` });
+  });
+  server.delete("/api/v1/movies/:id", (req, res) => {
+    const { id } = req.params;
+    res.json({ message: ` deleting post id;${id}` });
+  });
+  // server.get("/faq", (req, res) => {
+  //   res.send(
+  //     `<html>
+  //       <head>
+  //         <body>
+  //           <h1>hello world</h1>
+  //         </body>
+  //       </head>
+  //     </html>`
+  //   );
+  // });
+
   //    we are handling all of the request coming to our server by *
   server.get("*", (req, res) => {
     // nextjs is handling req and providing pages wherewe are navigating
